b45006a366dfd333f4a97b74b09121f79191e964
7i7o <mparij@gmail.com>
2022-05-12 16:19:46 -0300

    getColors done

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,52 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.10;
+pragma solidity 0.8.12;
 
-contract Contract {}
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract Contract {
+    function uint8tohexchar(uint8 i) private pure returns (uint8) {
+        return (i > 9) ? (i + 87) : (i + 48); // (i > 9) ? (ascii a-f) : (ascii 0-9)
+    }
+
+    function fixOpacity(uint32 i) private pure returns (string memory) {
+        bytes memory o = new bytes(8);
+        uint32 mask = 0x0000000f;
+        uint32 mask16 = 0x000000ff;
+        uint32 j = (uint16(i & mask16) * 256) / 1024 + 191;
+        o[7] = bytes1(uint8tohexchar(uint8(j & mask)));
+        j = j >> 4;
+        o[6] = bytes1(uint8tohexchar(uint8(j & mask)));
+        i = i >> 8;
+        o[5] = bytes1(uint8tohexchar(uint8(i & mask)));
+        i = i >> 4;
+        o[4] = bytes1(uint8tohexchar(uint8(i & mask)));
+        i = i >> 4;
+        o[3] = bytes1(uint8tohexchar(uint8(i & mask)));
+        i = i >> 4;
+        o[2] = bytes1(uint8tohexchar(uint8(i & mask)));
+        i = i >> 4;
+        o[1] = bytes1(uint8tohexchar(uint8(i & mask)));
+        i = i >> 4;
+        o[0] = bytes1(uint8tohexchar(uint8(i & mask)));
+        return string(o);
+    }
+
+    function getColors(address _addr) public pure returns(string[4] memory) {
+        string[4] memory s;
+        uint256 h = uint(keccak256(abi.encodePacked(_addr)));
+        uint256 mask = 0x000000000000000000000000ffffffff;
+        h = h >> 128;
+        s[3] = fixOpacity(uint32(h & mask));
+        h = h >> 32;
+        s[2] = fixOpacity(uint32(h & mask));
+        h = h >> 32;
+        s[1] = fixOpacity(uint32(h & mask));
+        h = h >> 32;
+        s[0] = fixOpacity(uint32(h & mask));
+        return s;
+    }
+
+    function getUintAsStr(uint256 num) public pure returns (string memory) {
+        return Strings.toString(num);
+    }
+}
