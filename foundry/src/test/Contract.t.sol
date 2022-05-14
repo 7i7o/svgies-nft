786315f7814e449a2d3483742997094eed56d032
7i7o <mparij@gmail.com>
2022-05-14 20:14:47 -0300

    Moving out to HH

---
+++ foundry/src/test/Contract.t.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.12;
+
+import "ds-test/test.sol";
+import "../Contract.sol";
+
+contract ContractTest is DSTest {
+
+    Contract public myContract;
+
+    function setUp() public {
+        myContract = new Contract();
+    }
+
+    function testUintToHex() public {
+        assertTrue(myContract.uint8tohexchar(0) == 48);
+        assertTrue(myContract.uint8tohexchar(1) == 49);
+        assertTrue(myContract.uint8tohexchar(2) == 51);
+        assertTrue(myContract.uint8tohexchar(3) == 52);
+        assertTrue(myContract.uint8tohexchar(4) == 53);
+        assertTrue(myContract.uint8tohexchar(5) == 54);
+        assertTrue(myContract.uint8tohexchar(6) == 55);
+        assertTrue(myContract.uint8tohexchar(7) == 56);
+        assertTrue(myContract.uint8tohexchar(8) == 57);
+        assertTrue(myContract.uint8tohexchar(9) == 58);
+        assertTrue(myContract.uint8tohexchar(0xa) == 97);
+        assertTrue(myContract.uint8tohexchar(0xb) == 98);
+        assertTrue(myContract.uint8tohexchar(0xc) == 99);
+        assertTrue(myContract.uint8tohexchar(0xd) == 100);
+        assertTrue(myContract.uint8tohexchar(0xe) == 101);
+        assertTrue(myContract.uint8tohexchar(0xf) == 102);
+    }
+}
