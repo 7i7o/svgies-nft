a5c6f233edf07f1cc4fb36804a6757a5894a174d
7i7o <mparij@gmail.com>
2022-05-14 01:40:20 -0300

    Paths, one step closer

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.12;
 
-import "@openzeppelin/contracts/utils/Strings.sol";
-
 contract Contract {
     function uint8tohexchar(uint8 i) private pure returns (uint8) {
         return (i > 9) ? (i + 87) : (i + 48); // (i > 9) ? (ascii a-f) : (ascii 0-9)
@@ -46,24 +44,62 @@ contract Contract {
         return s;
     }
 
-    function getUintAsStr(uint256 num) public pure returns (string memory) {
-        return Strings.toString(num);
+    function toString(uint8 value) internal pure returns (string memory) {
+        // Inspired by OpenZeppelin's implementation - MIT licence
+        // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Strings.sol
+        if (value == 0) {
+            return "0";
+        }
+        if (value < 10) {
+            bytes memory buffer1 = new bytes(1);
+            buffer1[0] = (bytes1(uint8(48 + value)));
+            return string(buffer1);
+        }
+        if (value < 100) {
+            bytes memory buffer2 = new bytes(2);
+            buffer2[1] = bytes1(uint8(48 + value % 10));
+            buffer2[0] = bytes1(uint8(48 + (value/10) % 10));
+            return string(buffer2);
+        }
+        bytes memory buffer3 = new bytes(3);
+        buffer3[2] = bytes1(uint8(48 + value % 10));
+        buffer3[1] = bytes1(uint8(48 + (value/10) % 10));
+        buffer3[0] = bytes1(uint8(48 + (value/100) % 10));
+        return string(buffer3);
     }
 
-    function getAddressCoordinates(address _addr) public pure returns (uint8[40] memory) {
+    function getCoordinates(address _addr) internal pure returns (uint8[40] memory) {
         uint8[40] memory o;
         uint160 a = uint160(address(_addr));
         uint160 mask = 15;
         for (uint256 i = 39; i > 0; i--) {
-            o[i] = uint8(a & mask);
+            o[i] = uint8((a & mask) + 16);
             a = a >> 4;
         }
         return o;
     }
 
     function getPath(address _addr) public pure returns (string memory) {
-        string memory o = 'M8 8';
-        o = string.concat(o,"z");
-        return o;
+        uint8[40] memory c = getCoordinates(_addr);
+        string memory y1 = toString(c[1]);
+        string memory y2 = toString(c[3]);
+        string memory o = string.concat('M',toString(c[0]),' ',y1,'C',toString(c[2]),' ',y2,' ');
+        string memory q = string.concat('M',toString(48-c[0]),' ',y1,'C',toString(48-c[2]),' ',y2,' ');
+        y1 = toString(c[5]);
+        y2 = toString(c[7]);
+        o = string.concat(o,toString(c[4]),' ',y1,' ',toString(c[6]),' ',y2);
+        q = string.concat(q,toString(48-c[4]),' ',y1,' ',toString(48-c[6]),' ',y2);
+        for (uint8 i = 8; i < 40; i+=4) {
+            y1 = toString(c[i+1]);
+            y2 = toString(c[i+3]);
+            o = string.concat(o,'S',toString(c[i]),' ',y1,' ',toString(c[i+2]),' ',y2);
+            q = string.concat(q,'S',toString(48-c[i]),' ',y1,' ',toString(48-c[i+2]),' ',y2);
+        }
+        y1 = toString(2*c[39]-c[37]);
+        y2 = toString(c[1]);
+        o = string.concat(o,'Q',toString(2*c[38]-c[36]),' ',y1,' ',toString(c[0]),' ',y2);
+        q = string.concat(q,'Q',toString(48-(2*c[38]-c[36])),' ',y1,' ',toString(48-c[0]),' ',y2);
+        return string.concat(o,q,"z");
     }
+
 }
